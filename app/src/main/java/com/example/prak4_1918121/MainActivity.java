e5e806ca8e2ded3edfcce3bc8974c3b9ac0c56cb
Raamadhan121 <ilhamcodet023@gmail.com>
2021-10-25 21:04:53 +0700

    Initial commit

---
+++ app/src/main/java/com/example/prak4_1918121/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.prak4_1918121;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private MahasiswaAdapter adapter;
+    private ArrayList<Mahasiswa> mahasiswaArrayList;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        addData();
+        recyclerView = (RecyclerView)findViewById(R.id.recycleview);
+        adapter = new MahasiswaAdapter(mahasiswaArrayList);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+    }
+    private void addData() {
+        mahasiswaArrayList = new ArrayList<>();
+        mahasiswaArrayList.add(new Mahasiswa("Ramadhan", "1918121", "2021"));
+        mahasiswaArrayList.add(new Mahasiswa("Ilhamsyah", "1918121", "2021"));
+        mahasiswaArrayList.add(new Mahasiswa("Nurendro", "1918121", "2021"));
+    }
+}
